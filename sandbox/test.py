70c8baf205eb8f6cd7348ac2c3480a257da2acd2
Katzu <109731414+k7tzu@users.noreply.github.com>
2025-01-27 15:38:00 +0100

    Update test.py

--- sandbox/test.py
+++ sandbox/test.py
@@ -1,26 +1,38 @@
-# Sujet Metropole 2023 J1 - Exercice 3
 class Region:
     def __init__(self, nom_region):
         self.nom = nom_region
         self.tab_voisines = []
         self.tab_couleurs_disponibles = ['Rouge', 'Vert', 'Bleu', 'Jaune', 'Orange', 'Marron']
         self.couleur_attribuee = None
+
     def renvoie_premiere_couleur_disponible(self):
-        return ...
-    def renvoie_nb_voisines(self) :
-        return ...
+        couleurs_utilisees = {voisine.couleur_attribuee for voisine in self.tab_voisines if voisine.couleur_attribuee}
+        for couleur in self.tab_couleurs_disponibles:
+            if couleur not in couleurs_utilisees:
+                return couleur
+        return None
+
+    def renvoie_nb_voisines(self):
+        return len(self.tab_voisines)
+
     def est_coloriee(self):
-        return ...
+        return self.couleur_attribuee is not None
+
     def retire_couleur(self, couleur):
-        ...
+        if couleur in self.tab_couleurs_disponibles:
+            self.tab_couleurs_disponibles.remove(couleur)
+
     def est_voisine(self, region):
-        ...
+        return region in self.tab_voisines
+
 
 class Pays:
     def __init__(self):
         self.tab_regions = [aqui, occi, paca, auvergne, ile, hauts, ge, bre, norm, loire, centre, bourg, corse]
+
     def renvoie_tab_regions_non_coloriees(self):
-        ...
+        return [region for region in self.tab_regions if not region.est_coloriee()]
+
     def renvoie_max(self):
         nb_voisines_max = -1
         region_max = None
@@ -29,10 +41,16 @@ class Pays:
                 nb_voisines_max = reg.renvoie_nb_voisines()
                 region_max = reg
         return region_max
-    def colorie(self):
-        ...
 
-# instanciation des 13 régions
+    def colorie(self):
+        while self.renvoie_tab_regions_non_coloriees():
+            region = self.renvoie_max()
+            if region:
+                couleur = region.renvoie_premiere_couleur_disponible()
+                if couleur:
+                    region.couleur_attribuee = couleur
+
+
 aqui = Region('Nouvelle-Aquitaine')
 occi = Region('Occitanie')
 paca = Region("Provence-Alpes-Côte-d-Azur")
@@ -47,7 +65,6 @@ centre = Region('Centre-Val-De-Loire')
 bourg = Region('Bourgogne-Franche-Comté')
 corse = Region('Corse')
 
-# affectations des régions voisines
 aqui.tab_voisines = [loire, centre, auvergne, occi]
 occi.tab_voisines = [aqui, auvergne, paca]
 paca.tab_voisines = [occi, auvergne]
@@ -62,15 +79,8 @@ centre.tab_voisines = [ile, bourg, auvergne, aqui, loire, norm]
 bourg.tab_voisines = [auvergne, ge, ile, centre]
 corse.tab_voisines = []
 
-# instanciation de la France
 France = Pays()
-
-# coloriage des régions
 France.colorie()
 
-# vérification des couples Région->Couleur
-for r in France.tab_regions : 
+for r in France.tab_regions:
     print(r.nom, "->", r.couleur_attribuee)
-
-
-
