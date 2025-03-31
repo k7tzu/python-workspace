3a7edd727eafdd0a2ed5479a48a04198d2e5de2a
Katzu <109731414+k7tzu@users.noreply.github.com>
2025-03-31 15:30:37 +0200

    Create temp.py

---
+++ archive/temp.py
@@ -0,0 +1,12 @@
+def detection_cycle(g:Graphe, en_cours:list, termines:list, depart:str) -> bool:
+    if depart in  ... :
+        return False
+    elif depart in ... :
+        return True
+    else:
+        ... .append(depart)
+        for voisin in g.voisins(depart):
+            if ... :
+                return True
+        ... .append(depart)
+        return False
