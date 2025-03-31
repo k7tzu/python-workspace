c6fbe1c15fa493f37fd16938511c0ba272675064
Katzu <109731414+k7tzu@users.noreply.github.com>
2025-03-31 15:35:13 +0200

    Update temp.py

--- archive/temp.py
+++ archive/temp.py
@@ -10,3 +10,17 @@ def detection_cycle(g:Graphe, en_cours:list, termines:list, depart:str) -> bool:
                 return True
         ... .append(depart)
         return False
+
+def detection_cycle(g: Graphe, en_cours: list, termines: list, depart: str) -> bool:
+    if depart in termines:
+        return False
+    elif depart in en_cours:
+        return True
+    else:
+        en_cours.append(depart)
+        for voisin in g.voisins(depart):
+            if detection_cycle(g, en_cours, termines, voisin):
+                return True
+        en_cours.remove(depart)
+        termines.append(depart)
+        return False
