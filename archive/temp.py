a28ada564ae35266acdbdc74fbe1d6b64b12df5a
Katzu <109731414+k7tzu@users.noreply.github.com>
2025-04-02 08:20:32 +0200

    Update temp.py

--- archive/temp.py
+++ archive/temp.py
@@ -1,26 +1,38 @@
-def detection_cycle(g:Graphe, en_cours:list, termines:list, depart:str) -> bool:
-    if depart in  ... :
-        return False
-    elif depart in ... :
-        return True
-    else:
-        ... .append(depart)
-        for voisin in g.voisins(depart):
-            if ... :
-                return True
-        ... .append(depart)
-        return False
+class Graphe:
+    def __init__(self, liste_sommets):
+        self.liste_sommets = []
+        self.adjacents = {}
+        
+    def ajoute_arete(self, sommetA, sommetB):
+        if sommetA not in self.liste_sommets:
+            self.liste_sommets.append(sommetA)
+            self.adjacents[sommetA] = []
+        if sommetB not in self.liste_sommets:
+            self.liste_sommets.append(sommetB)
+            self.adjacents[sommetB] = []
+        self.adjacents[sommetA].append(sommetB)
+        self.adjacents[sommetB].append(sommetA)
+        
+    def voisins(self, sommet):
+        return self.adjacents.get(sommet, [])
+        
+g = Graphe(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'])
+g.ajoute_arete('A', 'B')
+g.ajoute_arete('A', 'C')
+g.ajoute_arete('B', 'D')
+g.ajoute_arete('D', 'C')
+g.ajoute_arete('B', 'E')
+g.ajoute_arete('D', 'E')
+g.ajoute_arete('E', 'F')
+g.ajoute_arete('E', 'G')
+g.ajoute_arete('F', 'G')
+g.ajoute_arete('G', 'H')
 
-def detection_cycle(g: Graphe, en_cours: list, termines: list, depart: str) -> bool:
-    if depart in termines:
-        return False
-    elif depart in en_cours:
-        return True
-    else:
-        en_cours.append(depart)
-        for voisin in g.voisins(depart):
-            if detection_cycle(g, en_cours, termines, voisin):
-                return True
-        en_cours.remove(depart)
-        termines.append(depart)
-        return False
+def DFSrec(g, traites, actuel):
+    traites.append(actuel)
+    for voisin in g.voisins(actuel):
+        if voisin not in traites:
+            DFSrec(g, traites, voisin)
+    return traites
+
+print(DFSrec(g, [], 'A'))
