e992630509d87bc3c79f3cb0f77783c4175bca6d
Katzu <109731414+k7tzu@users.noreply.github.com>
2025-04-02 09:50:50 +0200

    Update temp.py

--- archive/temp.py
+++ archive/temp.py
@@ -1,47 +1,54 @@
-def detection_cycle(g:Graphe, en_cours:list, termines:list, depart:str) -> bool:
-    if depart in termines:
-        return False
-    elif depart in en_cours:
-        return True
-    else:
-        en_cours.append(depart)
-        for voisin in g.voisins(depart):
-            if detection_cycle(g, en_cours, termines, voisins):
-                return True
-        termines.append(depart)
-        return False
-
-
 class Graphe:
-    def __init__(self, sommets, aretes):
-        """
-        sommets : liste des sommets du graphe.
-        aretes : liste de tuples représentant les arêtes (u, v).
-        """
-        self.sommets = sommets
-        self.adjacence = {s: [] for s in sommets}
-        for u, v in aretes:
-            self.adjacence[u].append(v)
-            self.adjacence[v].append(u)  # Supposons un graphe non orienté
-
+    def __init__(self, liste_sommets):
+        self.liste_sommets = []
+        self.adjacents = {}
+        
+    def ajoute_arete(self, sommetA, sommetB):
+        if sommetA not in self.liste_sommets:
+            self.liste_sommets.append(sommetA)
+            self.adjacents[sommetA] = []
+        if sommetB not in self.liste_sommets:
+            self.liste_sommets.append(sommetB)
+            self.adjacents[sommetB] = []
+        self.adjacents[sommetA].append(sommetB)
+        self.adjacents[sommetB].append(sommetA)
+        
     def voisins(self, sommet):
-        """Retourne la liste des voisins du sommet."""
-        return self.adjacence[sommet]
+        return self.adjacents.get(sommet, [])
+    
+    def est_connexe(self):
+        depart = g.liste_sommets[0]
+        parcours = BFS(g, depart)
+        return len(g.liste_sommets) == len(parcours)
+    
+    def est_eulerien(self):
+        total = 0
+        for elt in g.liste_sommets:
+            if len(g.voisins(elt)) % 2 == 1:
+                total += 1
+        return est_connexe(g) and (total == 0 or total == 2)
+                
+g1 = Graphe(['A', 'B', 'C', 'D', 'E'])
+g1.ajoute_arete('A', 'B')
+g1.ajoute_arete('A', 'C')
+g1.ajoute_arete('A', 'E')
+g1.ajoute_arete('A', 'D')
+g1.ajoute_arete('C', 'E')
+g1.ajoute_arete('E', 'D')
 
-    def est_connexe(self) -> bool:
-        """Détermine si le graphe est connexe."""
-        if not self.sommets:
-            return True  # Un graphe vide est considéré comme connexe
-        
-        visite = set()
-        pile = [self.sommets[0]]
-        
-        while pile:
-            courant = pile.pop()
-            if courant not in visite:
-                visite.add(courant)
-                for voisin in self.voisins(courant):
-                    if voisin not in visite:
-                        pile.append(voisin)
-        
-        return len(visite) == len(self.sommets)
+
+def BFS(g, depart):
+        traites = []
+        decouverts = [depart]
+        en_attente = [depart]
+        while en_attente != []:
+            sommet = en_attente.pop(0)
+            voisins = g.voisins(sommet)
+            for voisin in voisins:
+                if voisin not in decouverts:
+                    decouverts.append(voisin)
+                    en_attente.append(voisin)
+            traites.append(sommet)
+        return traites
+    
+print(BFS(g1, 'A'))
