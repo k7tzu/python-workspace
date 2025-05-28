db474615a0ad46f11f7f80bcb104d476538b063e
k7tzu <lnx.vrb@gmail.com>
2025-05-28 15:01:35 +0200

    update fibo for recursion

--- bns/sub-3/exercise-1.py
+++ bns/sub-3/exercise-1.py
@@ -12,6 +12,20 @@ def fibonacci(n):
         x, y = y, x + y
     return x
 
+def fibo_recursion(n):
+    """
+    >>> fibo_recursion(1)
+    1
+    >>> fibo_recursion(2)
+    1
+    >>> fibo_recursion(25)
+    75025
+    """
+    if n <= 1:
+        return n
+    else:
+        return (fibo_recursion(n-1) + fibo_recursion(n-2))
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
